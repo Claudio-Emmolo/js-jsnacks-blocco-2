3ad5a16576e94d93ae34b13d0f09c10effc2b6f3
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-25 16:48:52 +0100

    Snack 3 - Somma

---
+++ js/snack-3.js
@@ -0,0 +1,26 @@
+// Calcola la somma dei primi 10 numeri di un array.
+// Calcola la somma e la media dei primi 10 numeri di un array.
+
+const numbers = [
+    2,
+    2,
+    2,
+    5,
+    2,
+    2,
+    8,
+    2,
+    2,
+    2
+]
+
+let calc = 0;
+
+let media = 0;
+
+for (let i = 0 ; i < 10 ; i++){
+    calc += numbers[i];
+    media = calc / 10;
+}
+
+console.log(media);
