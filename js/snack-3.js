988e1acf662ead1b273a0ba77a3d7ccce7af7828
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-11-25 17:34:23 +0100

    Snack-3 Max Value

--- js/snack-3.js
+++ js/snack-3.js
@@ -1,5 +1,6 @@
 // Calcola la somma dei primi 10 numeri di un array.
 // Calcola la somma e la media dei primi 10 numeri di un array.
+// Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
 
 const numbers = [
     2,
@@ -18,11 +19,18 @@ let calc = 0;
 
 let media = 0;
 
+let maxValue = numbers[0];
+
 for (let i = 0 ; i < 10 ; i++){
     calc += numbers[i];
+
+    if (maxValue < numbers[i]){
+        maxValue = numbers[i];
+    }
 }
 
 media = calc / 10;
 
 console.log(calc);
 console.log(media);
+console.log(maxValue);
